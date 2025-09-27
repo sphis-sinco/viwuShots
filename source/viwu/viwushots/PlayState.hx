b0815771af3a04ea0facbf6ba4dfcdae4fb9ec76
Viwu <viwuwevi@gmail.com>
2025-09-27 10:53:42 -0500

    ghost viwu for offsets

--- source/viwu/viwushots/PlayState.hx
+++ source/viwu/viwushots/PlayState.hx
@@ -5,10 +5,15 @@ import flixel.FlxState;
 class PlayState extends FlxState
 {
 	public var viwu:Viwu;
+	public var viwu_ghost:Viwu;
 
 	override public function create()
 	{
 		super.create();
+
+		viwu_ghost = new Viwu();
+		add(viwu_ghost);
+		viwu_ghost.alpha = .5;
 
 		viwu = new Viwu();
 		add(viwu);
