dd80296534e36ec7b583a0d2a74c81a05816f49f
Viwu <viwuwevi@gmail.com>
2025-09-27 11:23:59 -0500

    focus mode

--- source/viwu/viwushots/PlayState.hx
+++ source/viwu/viwushots/PlayState.hx
@@ -3,12 +3,16 @@ package viwu.viwushots;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	public var viwu:Viwu;
 	public var floor:FlxSprite;
+
+	public var focusMode:Bool = false;
 
 	override public function create()
 	{
@@ -22,10 +26,32 @@ class PlayState extends FlxState
 
 		floor.setPosition(-275.95, 437.25);
 		viwu.setPosition(-59.7, 65.2);
+
+		FlxG.camera.zoom = 1;
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.keys.justReleased.F && !focusMode)
+		{
+			focusMode = true;
+			viwu.animation.play('focus');
+
+			FlxTween.tween(FlxG.camera, {zoom: 1.1}, 1, {
+				ease: FlxEase.sineInOut
+			});
+		}
+
+		if (FlxG.keys.justReleased.F && focusMode)
+		{
+			focusMode = false;
+			viwu.animation.play('unfocus');
+
+			FlxTween.tween(FlxG.camera, {zoom: 1}, 1, {
+				ease: FlxEase.sineInOut
+			});
+		}
 	}
 }
