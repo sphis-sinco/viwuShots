ae5310db745f49b2c6099768370a73ef57e38bec
Viwu <viwuwevi@gmail.com>
2025-09-27 10:48:23 -0500

    Viwu asset loading

--- source/viwu/viwushots/Viwu.hx
+++ source/viwu/viwushots/Viwu.hx
@@ -1,11 +1,40 @@
 package viwu.viwushots;
 
 import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxPoint;
 
 class Viwu extends FlxSprite
 {
+	public var offsets:Map<String, FlxPoint> = [];
+
 	override public function new()
 	{
 		super();
+
+		offsets.clear();
+		loadAsset();
+	}
+
+	public function loadAsset()
+	{
+		frames = FlxAtlasFrames.fromSparrow('assets/images/viwu.png', 'assets/images/viwu.xml');
+
+		animation.addByPrefix('idle', 'viwu animation idle', 24, true);
+		animation.addByPrefix('focus', 'viwu animation focus', 24, false);
+		animation.addByPrefix('unfocus', 'viwu animation unfocus', 24, false);
+		animation.addByPrefix('shoot', 'viwu animation shoot', 24, false);
+
+		offsets.set('idle', new FlxPoint(0, 0));
+		offsets.set('focus', new FlxPoint(0, 0));
+		offsets.set('unfocus', new FlxPoint(0, 0));
+		offsets.set('shoot', new FlxPoint(0, 0));
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		offset.set(offsets.get(animation.name).x, offsets.get(animation.name).y);
 	}
 }
