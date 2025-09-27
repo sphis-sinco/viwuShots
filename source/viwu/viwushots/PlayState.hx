50f41db7c87af689ff6435b5940a23c1555ae913
Viwu <viwuwevi@gmail.com>
2025-09-27 11:21:28 -0500

    use fla positions

--- source/viwu/viwushots/PlayState.hx
+++ source/viwu/viwushots/PlayState.hx
@@ -19,6 +19,9 @@ class PlayState extends FlxState
 
 		floor = new FlxSprite().loadGraphic('assets/images/floor.png');
 		add(floor);
+
+		floor.setPosition(-275.95, 437.25);
+		viwu.setPosition(-59.7, 65.2);
 	}
 
 	override public function update(elapsed:Float)
