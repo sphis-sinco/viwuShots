7e7251a61952a3f6cf9c63013ea39e1f0cdf2836
Viwu <sinconsistencia@gmail.com>
2025-09-27 15:20:55 -0500

    gameover substate

--- source/sphis/viwushots/GameOverSubState.hx
+++ source/sphis/viwushots/GameOverSubState.hx
@@ -1,21 +1,40 @@
 package sphis.viwushots;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxSubState;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
 class GameOverSubState extends FlxSubState
 {
+	public var gameover:FlxSprite;
+	public var retry:FlxSprite;
+
 	override function create()
 	{
 		super.create();
+
+		gameover = new FlxSprite();
+		gameover.loadGraphic('assets/images/gameover.png');
+		gameover.alpha = 0;
+
+		retry = new FlxSprite();
+		retry.loadGraphic('assets/images/retry.png');
+		retry.alpha = 0;
+
+		add(gameover);
+		add(retry);
+
+		FlxTween.tween(gameover, {alpha: 1}, 1);
+		FlxTween.tween(retry, {alpha: 1}, 1);
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		if (FlxG.keys.justReleased.ENTER)
+		if (FlxG.mouse.justReleased && FlxG.mouse.overlaps(retry))
 		{
 			FlxG.camera.fade(FlxColor.BLACK, 1, false, () ->
 			{
