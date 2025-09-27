052a53578fe4556814b3856068c1bc01ec578f6e
Viwu <viwuwevi@gmail.com>
2025-09-27 11:18:53 -0500

    ViwuShootAnimationOffsetsState define

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		#if ViwuShootAnimationOffsetsState
+		addChild(new FlxGame(0, 0, viwu.viwushots.ViwuShootAnimationOffsetsState));
+		#else
 		addChild(new FlxGame(0, 0, viwu.viwushots.PlayState));
+		#end
 	}
 }
